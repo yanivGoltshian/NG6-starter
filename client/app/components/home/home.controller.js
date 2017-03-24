b145c19c3c554e72a5dda229b03b70e4ed871968
lysk88 <lysk88@gmail.com>
2017-03-24 08:51:41 +0300

    ll

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -31,13 +31,13 @@ class HomeController {
             let type = fbData[key].type;
             var src = fbData[key].src;
             if (type === 'text') {
-                let paragraph = JSON.parse(JSON.stringify(window.paragraphSection[0]));
+                let paragraph = window.paragraphSection[0];
                 paragraph.text.ops[0].insert=src;
                 paragraph.id = this.guid();
                 window.storyItItem["sections"].push([paragraph]);
             }
             else if( type === 'image') {
-                let img = JSON.parse(JSON.stringify(window.imgSection[0]));
+                let img = window.imgSection[0];
                 img.media.originalImageUrl=src;
                 img.media.url=src;
                 img.id = this.guid();
@@ -55,7 +55,7 @@ class HomeController {
         withCredentials:true
       };
 
-      window.storyItItem["id"]=this.guid();
+      window.storyItItem["id"]= this.guid();
       this.http(req).then(() => {
         this.hideLoader()
       }, () => {
